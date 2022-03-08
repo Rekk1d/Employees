fcd4b812f49241606196c398cce634113999f13c
Misha <xxxdaddy420@gmail.com>
2022-03-08 19:13:29 +0700

    fixed bug with key in EmployeesListItem

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,11 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
+    const data = [
+        {name: 'William J.', salary: 1500, increase: false, id: 1},
+        {name: 'Elizabeth H.', salary: 2100, increase: true, id: 2},
+        {name: 'Carl S.', salary: 900, increase: false, id: 3}
+    ]
     return (
         <div className='app'>
             <AppInfo/>
@@ -13,7 +18,7 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     );
