c0c6201517f262849a697dc7f7026e41a4acea5c
Misha <xxxdaddy420@gmail.com>
2022-03-12 19:19:57 +0700

    work with app-info,  optimization onIncrease and onRise function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,9 +13,9 @@ class App extends Component {
         super(props)
         this.state = {
             data: [
-                {name: 'William J.', salary: 1500, increase: false, id: 1},
-                {name: 'Elizabeth H.', salary: 2100, increase: true, id: 2},
-                {name: 'Carl S.', salary: 900, increase: false, id: 3}
+                {name: 'William J.', salary: 1500, increase: false, rise: false, id: 1},
+                {name: 'Elizabeth H.', salary: 2100, increase: false, rise: false, id: 2},
+                {name: 'Carl S.', salary: 900, increase: false, rise: false, id: 3}
             ]
         }
         this.maxId = 4;
@@ -42,17 +42,34 @@ class App extends Component {
             }
         })
     }
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if(item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                } return item;
+            })
+        }))
+    }
+   
     
     render() {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
         return (
             <div className='app'>
-                <AppInfo/>
+                <AppInfo
+                employees={employees}
+                increased={increased}
+                />
                 <div className="search-panel">
                     <SearchPanel/>
                     <AppFilter/>
                 </div>
                 <EmployeesList data={this.state.data}
-                onDelete={this.deleteItem}/>
+                onDelete={this.deleteItem}
+                onToggleProp={this.onToggleProp}
+                />
                 <EmployeesAddForm addItem={this.addItem}/>
             </div>
         );
